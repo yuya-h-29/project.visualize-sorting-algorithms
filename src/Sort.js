0ed903f253ce5fb0faf47dbaa81eedae1045154d
Yuya Harada <tzou11291996@gmail.com>
2020-04-14 00:40:21 +0900

    first commit

--- src/Sort.js
+++ src/Sort.js
@@ -1,7 +1,94 @@
-class SomeKindOfSort {
-  constructor(array) {
-    this.array = array;
+class Heap {
+  constructor() {
+    this.array = [];
+    this.left = undefined;
+    this.right = undefined;
+    this.size = 0;
+    this.childIdx = 0;
+    // this.parentIdx = Math.floor((this.childIdx - 1) / 2);
+    this.leftChildIdx = this.parentIdx * 2 + 1;
+    this.rightChildIdx = this.left + 1;
+    // let lastPosition = 0;
   }
+
+  insert(value) {
+    // console.log(this.array, value);
+    this.array[this.size] = value;
+    this.size++;
+    this.heapifyUp();
+    console.log(
+      "this is the current values in the array",
+      this.array,
+      this.size
+    );
+  }
+
+  heapifyUp() {
+    let index = this.size - 1;
+    let parentIdx = Math.floor((index - 1) / 2);
+    while (index >= 0 && this.array[index] < this.array[parentIdx]) {
+      this.swapValue(parentIdx, index);
+      index = parentIdx;
+      parentIdx = Math.floor((index - 1) / 2);
+    }
+  } // parent node exists and while parent value is bigger than its child, use swapValue() and change value;
+  swapValue(index1, index2) {
+    let parentNode = this.array[index2];
+    this.array[index2] = this.array[index1];
+    this.array[index1] = parentNode;
+  }
+
+  remove() {
+    if (this.size !== 0) {
+      console.log("SSSSSSSS", this.array);
+      let item = this.array[0];
+      this.array[0] = this.array[this.size - 1];
+      this.size--;
+      this.heapifyDown();
+      this.array.pop();
+      console.log("RRRRRR", this.array.length, this.array);
+      return item;
+    }
+  }
+
+  heapifyDown() {
+    let index = 0;
+    // let leftChildIdx = index * 2 + 1;
+    // let rightChildIdx = leftChildIdx + 1;
+    const getLeftChildIndex = (parent) => {
+      return 2 * parent + 1;
+    };
+    const getRightChlidIndex = (parent) => {
+      return 2 * parent + 2;
+    };
+    // const hasLeftChild = (index) => {
+    //   if (index < this.size) {
+    //     return true;
+    //   }
+    // };
+    const hasRightChild = (index) => {
+      if (index < this.size) {
+        return true;
+      }
+    };
+    while (index < this.size) {
+      this.childIdx = getLeftChildIndex(index);
+      if (
+        hasRightChild(getRightChlidIndex(index)) &&
+        this.array[getRightChlidIndex(index)] <
+          this.array[getLeftChildIndex(index)]
+      ) {
+        this.childIdx = getRightChlidIndex(index);
+      }
+      if (this.array[index] < this.array[this.childIdx]) {
+        break;
+      } else {
+        this.swapValue(index, this.childIdx);
+      }
+      index = this.childIdx;
+    }
+  }
+
   sort() {
     console.log("sort!");
   }
@@ -11,4 +98,11 @@ class SomeKindOfSort {
   }
 }
 
-module.exports = SomeKindOfSort;
+module.exports = Heap;
+
+// Heap Sort: Heap Sort uses Binary Heap to sort an array in O(nLogn) time.
+// Priority Queue: Priority queues can be efficiently implemented using Binary Heap because it supports insert(), delete() and extractmax(), decreaseKey() operations in O(logn) time.
+// Graph Algorithms: The priority queues are especially used in Graph Algorithms like Dijkstra’s Shortest Path and Prim’s Minimum Spanning Tree.
+
+//add
+//remove the root
--- test/_sort.js
+++ test/_sort.js
@@ -1,11 +1,88 @@
-const Sort = require("../src/Sort");
+const Heap = require("../src/Sort");
 const { expect } = require("chai");
 
-describe("Sort", () => {
+let heap;
+describe("Heap", () => {
+  beforeEach(() => {
+    heap = new Heap();
+  });
+
   it("should be a function", () => {
-    expect(Sort).to.be.a("function");
+    expect(Heap).to.be.a("function");
   });
   it("should have a sort method", () => {
-    expect(Sort.prototype.sort).to.be.a("function");
+    expect(Heap.prototype.sort).to.be.a("function");
   });
+
+  it("should have inset method", () => {
+    expect(heap.insert).to.be.a("function");
+  });
+
+  it("should return an array", () => {
+    const one = 1;
+    const two = 2;
+    heap.insert(one);
+    heap.insert(two);
+    expect(heap.array[0]).to.be.equal(1);
+    expect(heap.array[1]).to.be.equal(2);
+  });
+
+  it("should swap its parents value and child value if parents value is bigger than its child value", () => {
+    const one = 1;
+    const two = 2;
+    const three = 3;
+    heap.insert(two);
+    heap.insert(three);
+    heap.insert(one);
+    expect(heap.array[0]).to.be.equal(1);
+    expect(heap.array[1]).to.be.equal(3);
+    expect(heap.array[2]).to.be.equal(2);
+  });
+
+  it("should remove first node from array, bring the last element to index 0, and sort the array", () => {
+    const one = 1;
+    const four = 4;
+    const three = 3;
+    heap.insert(three);
+    heap.insert(four);
+    heap.insert(one);
+    let test = heap.remove();
+    expect(test).to.be.equal(1);
+    expect(heap.array[0]).to.be.equal(3);
+    expect(heap.array[1]).to.be.equal(4);
+    expect(heap.array.length).to.be.equal(2);
+  });
+
+  it("should remove first node from array, bring the last element to index 0, and sort the array", () => {
+    const nine = 9;
+    const four = 4;
+    const eleven = 11;
+    const twelve = 12;
+    const ten = 10;
+    const five = 5;
+    const three = 3;
+    heap.insert(nine);
+    heap.insert(four);
+    heap.insert(eleven);
+    heap.insert(twelve);
+    heap.insert(ten);
+    heap.insert(five);
+    heap.insert(three);
+    let test = heap.remove();
+    expect(test).to.be.equal(3);
+    expect(heap.array[0]).to.be.equal(4);
+    expect(heap.array[1]).to.be.equal(9);
+    expect(heap.array[2]).to.be.equal(5);
+    expect(heap.array.length).to.be.equal(6);
+  });
+
+  // it("should remove the first element of an array", ()=> {
+  //   const one = 1;
+  //   const two = 2;
+  //   const three = 3;
+  //   heap.insert(one);
+  //   heap.insert(two);
+  //   heap.insert(three);
+  //   heap
+  // });
 });
